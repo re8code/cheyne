9442016d6e8ca533828630d2c684ef85bcabe9f6
re8code <won@re8code.com>
2024-12-01 22:03:02 +0900

    set, map clear

--- 202202833_script.js
+++ 202202833_script.js
@@ -3,6 +3,7 @@ var bgstring = '';
 const forshow = 'click for show!!';
 var history_start_index = 0;
 var history_data = [];
+var b1st = true; // history rotation 버튼을 처음 클릭한다.
 
 function explain_show() {
   const explain = document.getElementsByClassName('background');
@@ -18,6 +19,11 @@ function explain_show() {
 }
 
 function art_rotate() {// history 카드를 로테이션 시킨다.
+  if (b1st) {
+    alert('History 카드가 이동합니다.');
+    b1st = false;
+  }
+
   let hs = [];
   for (let i=0; i<3; i++) {
     let query = '#history .history' + i + ' h3';
@@ -55,4 +61,28 @@ function history_initdata() {
 
 function alert_home() {
   alert('HOME으로 이동합니다.');
+}
+
+function table_initdata() {
+  let name_data = new Set(['Gimpo Airport Lotte Mall', 'Seoul Botanic Park', 'Heojun Museum']);
+  let name = document.getElementsByClassName('table_name');
+
+  let i=0;
+  for (const key of name_data.keys()) {
+    name[i++].textContent = key;
+  }
+
+  set_image('Shopping');
+  set_image('Park');
+  set_image('Museum');
+}
+
+function set_image(type) {
+  let resourses = new Map();
+  resourses.set('Shopping', './res/gimpo.webp');
+  resourses.set('Park', './res/botanic.webp');
+  resourses.set('Museum', './res/heojun.jpg');
+
+  let ele = document.querySelector('.' + type + ' img');
+  ele.src = resourses.get(type);
 }
