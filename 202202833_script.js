422648b16cf53ec975a2dc320e3da460328a9dae
re8code <won@re8code.com>
2024-12-01 21:11:32 +0900

    history clear 2

--- 202202833_script.js
+++ 202202833_script.js
@@ -1,12 +1,14 @@
-var bgcnt = 0;
+var bshow = true;
 var bgstring = '';
 const forshow = 'click for show!!';
+var history_start_index = 0;
+var history_data = [];
 
 function explain_show() {
   const explain = document.getElementsByClassName('background');
-  bgcnt = 1 - bgcnt;
+  bshow = !bshow;
 
-  if (bgcnt == 1) {
+  if (!bshow) {
     bgstring = explain[0].innerHTML;
     explain[0].innerHTML = forshow;
   }
@@ -15,26 +17,42 @@ function explain_show() {
   }
 }
 
-function art_rotate() {
-  content_swap(1, 2);
-  content_swap(2, 3);
+function art_rotate() {// history 카드를 로테이션 시킨다.
+  let hs = [];
+  for (let i=0; i<3; i++) {
+    let query = '#history .history' + i + ' h3';
+    hs.push(document.querySelector(query).innerHTML);
+  }
+
+  for (let i=0, si=1; i<3; i++, si++) {
+    let query = '#history .history' + (si % 3) + ' h3';
+    let ele = document.querySelector(query);
+    ele.innerHTML = hs[i];
+
+    let index = 0;
+    for (let j=0; j<3; j++) {
+      if (ele.innerHTML == history_data[j].key) {
+        index = j; break;
+      }
+    }
+    query = '#history .history' + (si % 3) + ' p';
+    ele = document.querySelector(query);
+    ele.innerHTML = history_data[index].value;
+  }
 }
 
-function content_swap(src, des) {
-  let query1 = '#history .history' + src;
-  let query2 = '#history .history' + des;
+function history_initdata() {
+  for (let i=0; i<3; i++) {
+    let key   = document.querySelector('#history .history' + i + ' h3').innerHTML;
+    let value = document.querySelector('#history .history' + i + ' p').innerHTML;
+    let obj = {};
+    obj.key = key;
+    obj.value = value;
+    history_data.push(obj);
+  }
+  console.log(history_data);
+}
 
-  let h1 = document.querySelector(query1 + ' h3');
-  let h2 = document.querySelector(query2 + ' h3');
-
-  let ts = h1.innerHTML;
-  h1.innerHTML = h2.innerHTML;
-  h2.innerHTML = ts;
-
-  let p1 = document.querySelector(query1 + ' p');
-  let p2 = document.querySelector(query2 + ' p');
-
-  let ps = p1.innerHTML;
-  p1.innerHTML = p2.innerHTML;
-  p2.innerHTML = ps;
+function alert_home() {
+  alert('HOME으로 이동합니다.');
 }
